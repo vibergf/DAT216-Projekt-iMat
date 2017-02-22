cb6b7a36f95afb16c0252a13179fa774f932ea61
KodarKooperativet <kodarkooperativet@gmail.com>
2017-02-22 10:42:04 +0100

    Added ListController

---
+++ iMat/src/IMat/ListController.java
@@ -0,0 +1,43 @@
+package IMat;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Erik on 2017-02-22.
+ */
+public class ListController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    @FXML
+    private void homeButtonPressed(){
+
+    }
+
+    @FXML
+    private void listButtonPressed(){
+
+    }
+
+    @FXML
+    private void historyButtonPressed(){
+
+    }
+
+    @FXML
+    private void cartButtonPressed(){
+
+    }
+
+    @FXML
+    private void checkoutButtonPressed(){
+
+    }
+}
