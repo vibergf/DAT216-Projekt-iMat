6aa35e42e4cfa88974e8bf23bd4d55c6fa812a5c
Erik Ramqvist <erikramqvist@gmail.com>
2017-02-26 11:18:05 +0100

    Dummy data into Shopping lists.

--- iMat/src/IMat/ListController.java
+++ iMat/src/IMat/ListController.java
@@ -1,7 +1,12 @@
 package IMat;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,9 +17,15 @@ import java.util.ResourceBundle;
 public class ListController implements Initializable {
 
 
+    private IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+
+    private @FXML
+    ListView<String> shoppingList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Börjes Kakor", "Vecko Listan", "Kanelbullar", "Kroppkakor", "Norrlands Pölsa");
+        shoppingList.setItems(items);
     }
 }
