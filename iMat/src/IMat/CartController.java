8e4faa5123d345d5d66f4afa81b5f691c9eff9a8
Emil <silvervibe@hotmail.com>
2017-02-28 21:06:51 +0100

    Updated the cart, added custom cells. For testing purposes only

--- iMat/src/IMat/CartController.java
+++ iMat/src/IMat/CartController.java
@@ -1,15 +1,25 @@
 package IMat;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Order;
+import se.chalmers.ait.dat215.project.ShoppingCart;
+import se.chalmers.ait.dat215.project.ShoppingItem;
 
 import java.awt.*;
 import java.awt.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static IMat.IMatController.formatPrice;
 
 /**
  * Created by Emil on 2017-02-22.
@@ -20,8 +30,20 @@ public class CartController implements Initializable {
     private javafx.scene.control.Label cartLabel;
     @FXML
     private javafx.scene.control.Label sumLabel;
+
     @FXML
-    private ListView cartList;
+    private TableView<ShoppingItem> cartItems;
+
+    @FXML
+    private TableColumn<ShoppingItem, ShoppingItem> nameColumn;
+    @FXML
+    private TableColumn<ShoppingItem, String> amountColumn;
+    @FXML
+    private TableColumn<ShoppingItem, String> priceColumn;
+    @FXML
+    private TableColumn<ShoppingItem, ShoppingItem> removeColumn;
+
+
     @FXML
     private Button checkoutButton;
     @FXML
@@ -29,10 +51,48 @@ public class CartController implements Initializable {
     @FXML
     private Button saveListButton;
 
+    private IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+       // dataHandler.getShoppingCart();
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(2));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(11));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(21));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(26));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(30));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(8));
+        dataHandler.getShoppingCart().addProduct(dataHandler.getProduct(19));
 
+        nameColumn.setCellFactory(new Callback<TableColumn<ShoppingItem, ShoppingItem>, TableCell<ShoppingItem, ShoppingItem>>() {
+            @Override
+            public TableCell<ShoppingItem, ShoppingItem> call(TableColumn<ShoppingItem, ShoppingItem> param) {
+                TableCell<ShoppingItem, ShoppingItem> cell = new CartTableCell();
+                return cell;
+            }
+        });
+        nameColumn.setCellValueFactory(c-> new SimpleObjectProperty<ShoppingItem>(c.getValue()));
+        amountColumn.setCellValueFactory(c-> new SimpleStringProperty((int)c.getValue().getAmount() + ""));
+        priceColumn.setCellValueFactory(c-> new SimpleStringProperty(formatPrice(c.getValue().getTotal())));
+
+
+
+        cartItems.setItems(FXCollections.observableList(dataHandler.getShoppingCart().getItems()));
+
+        sumLabel.setText("Summa: " + formatPrice(dataHandler.getShoppingCart().getTotal()));
+        //cartItems.setItems();
     }
+
+    @FXML
+    public void clearCartButtonPressed() {
+        dataHandler.getShoppingCart().clear();
+    }
+
+    @FXML
+    public void saveListButtonPressed(){
+    }
+
 
     //test
     @FXML public void checkoutButtonPressed(){
---
+++ iMat/src/IMat/CartTableCell.java
@@ -0,0 +1,54 @@
+package IMat;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.ShoppingItem;
+
+import java.io.IOException;
+
+/**
+ * Created by Emil on 2017-02-28.
+ */
+public class CartTableCell extends TableCell<ShoppingItem, ShoppingItem> {
+    @FXML
+    protected Label productLabel;
+    @FXML protected ImageView productThumbnail;
+
+    @FXML protected HBox hBox;
+
+    private FXMLLoader loader;
+
+    private static final int THUMBNAIL_SIZE = 50;
+
+    public CartTableCell(){
+        loader = new FXMLLoader(getClass().getResource("/cartTableCell.fxml"));
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
+
+    @Override
+    public void updateItem(ShoppingItem item, boolean empty){
+//        super.updateItem(item, empty);
+
+        if (!(empty || item == null)) {
+            setContent(item);
+            setGraphic(hBox);
+        }else{
+            setGraphic(null);
+        }
+    }
+
+
+    private void setContent(ShoppingItem item) {
+        productLabel.setText(item.getProduct().getName());
+        productThumbnail.setImage(IMatDataHandler.getInstance().getFXImage(item.getProduct(), THUMBNAIL_SIZE, THUMBNAIL_SIZE));
+    }
+}
