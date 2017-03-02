7a7c66691465a3f373cb40973bd6433351f2721a
Erik Ramqvist <erikramqvist@gmail.com>
2017-03-02 10:48:15 +0100

    Minor tweaks to shopping list cell. Fixed Save as Shopping list button in Cart.

--- iMat/src/IMat/CartController.java
+++ iMat/src/IMat/CartController.java
@@ -1,24 +1,30 @@
 package IMat;
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.*;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
+import javafx.util.Pair;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
 import se.chalmers.ait.dat215.project.Order;
 import se.chalmers.ait.dat215.project.ShoppingCart;
 import se.chalmers.ait.dat215.project.ShoppingItem;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static IMat.IMatController.formatPrice;
@@ -107,6 +113,66 @@ public class CartController implements Initializable {
 
         sumLabel.setText("Summa: " + formatPrice(dataHandler.getShoppingCart().getTotal()));
         //cartItems.setItems();
+
+        saveListButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Create the custom dialog.
+                Dialog<Pair<String, String>> dialog = new Dialog<>();
+                dialog.setTitle("Skapa Handlingslista");
+                dialog.setHeaderText("Ange titel och beskrivning nedan, tryck på Skapa när du är klar.");
+
+// Set the icon (must be included in the project).
+// Set the button types.
+                ButtonType loginButtonType = new ButtonType("Skapa", ButtonBar.ButtonData.OK_DONE);
+                dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+// Create the username and password labels and fields.
+                GridPane grid = new GridPane();
+                grid.setHgap(10);
+                grid.setVgap(10);
+                //grid.setPadding(new Insets(20, 150, 10, 10));
+
+                TextField username = new TextField();
+                username.setPromptText("Titel");
+                TextField password = new TextField();
+                password.setPromptText("Beskrivning");
+
+                grid.add(new Label("Titel:"), 0, 0);
+                grid.add(username, 1, 0, 3, 1);
+                grid.add(new Label("Beskrivning:"), 0, 1);
+                grid.add(password, 1, 1, 3, 3);
+
+// Enable/Disable login button depending on whether a username was entered.
+                Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+                loginButton.setDisable(true);
+
+// Do some validation (using the Java 8 lambda syntax).
+                username.textProperty().addListener((observable, oldValue, newValue) -> {
+                    loginButton.setDisable(newValue.trim().isEmpty());
+                });
+
+                dialog.getDialogPane().setContent(grid);
+
+// Request focus on the username field by default.
+                Platform.runLater(() -> username.requestFocus());
+
+// Convert the result to a username-password-pair when the login button is clicked.
+                dialog.setResultConverter(dialogButton -> {
+                    if (dialogButton == loginButtonType) {
+                        return new Pair<>(username.getText(), password.getText());
+                    }
+                    return null;
+                });
+
+                Optional<Pair<String, String>> result = dialog.showAndWait();
+
+                result.ifPresent(usernamePassword -> {
+                    System.out.println("Titel=" + usernamePassword.getKey() + ", Beskrining=" + usernamePassword.getValue());
+                    ShoppingList.addNewShoppingList(new ShoppingList(usernamePassword.getKey(), usernamePassword.getValue(), dataHandler.getShoppingCart().getItems()));
+                });
+            }
+        });
     }
 
     @FXML
